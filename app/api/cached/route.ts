d713982f5b39daf5d1ab1ee9ae0f22465fc380f5
Shea McKinney <sheamck@stanford.edu>
2025-05-19 22:51:43 -0700

    Testing again...

---
+++ app/api/cached/route.ts
@@ -0,0 +1,29 @@
+import { NextResponse } from 'next/server';
+
+/**
+ * Route to fetch all alerts from Storyblok.
+ * This is a server action that fetches all alerts from Storyblok and returns them as JSON.
+ */
+export async function GET() {
+  console.log('Running Cache function...');
+  const responseHash = {
+    message: 'The time is ' + new Date().toISOString(),
+  };
+
+  const headers = new Headers({
+    // Tell CDNs to cache the response.
+    'Netlify-CDN-Cache-Control': 'public, durable, max-age=3600, stale-while-revalidate=86400',
+    'CDN-Cache-Control': 'public, durable, max-age=3600, stale-while-revalidate=86400',
+    // Tell browsers not to cache the response.
+    'Cache-Control': 'public, max-age=0, must-revalidate',
+    // Tag the response for purging.
+    'Netlify-Cache-Tag': 'story',
+    'Cache-Tag': 'story',
+    // For testing purposes.
+    'x-test': new Date().toISOString(),
+  });
+
+  return NextResponse.json(responseHash, {
+    headers,
+  });
+}
